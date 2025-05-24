7414073efde32bad04080761ae9599e39c4f9340
Piyush Badhe <badhepiyuraj1997@gmail.com>
2025-05-24 21:13:45 +0530

    Basic express app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (_req, res) => {
+  console.log("Root route hit");
+  res.send("Hello from Express + TypeScript!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
